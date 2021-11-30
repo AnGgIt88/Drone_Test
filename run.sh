f10897b72665d5e8bf4f2ce27fbef11582eae4ee
AnGgIt88 <jarbull87@gmail.com>
2021-11-30 16:01:21 +0700

    Test Build Gcc arm64
    Signed-off-by: AnGgIt88 <jarbull87@gmail.com>

--- run.sh
+++ run.sh
@@ -4,17 +4,20 @@ alias gcc=gcc-10
 alias g++=g++-10
 git config --global user.name "${GITHUB_USER}"
 git config --global user.email "${GITHUB_EMAIL}"
-git clone https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/"${GITHUB_USER}"/gcc-arm ../gcc-arm -b gcc-master
-rm -rf ../gcc-arm/*
+git clone https://"${GITHUB_USER}":"${GITHUB_TOKEN}"@github.com/"${GITHUB_USER}"/gcc-arm64 ../gcc-arm64 -b gcc-master
+rm -rf ../gcc-arm64/*
+git checkout README.md # keep this as it's not part of the toolchain itself
 chmod a+x build-*.sh
-curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="<b>Starting ARM (32-bit) GCC Build</b>"
-./build-gcc.sh -a arm arm64
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="<b>Starting ARM64 (64-bit) GCC Build</b>"
+./build-gcc.sh -a arm64
 sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-./build-lld.sh -a arm arm64
-cd ../gcc-arm
-./bin/arm-eabi-gcc -v 2>&1 | tee /tmp/gcc-arm-version
-./bin/arm-eabi-ld.lld -v 2>&1 | tee /tmp/lld-arm-version
+./build-lld.sh -a arm64
+cd ../gcc-arm64
+./bin/aarch64-elf-gcc -v 2>&1 | tee /tmp/gcc-version
+./bin/aarch64-elf-ld.lld -v 2>&1 | tee /tmp/lld-arm64-version
 git add . -f
-git commit -as -m "Import ARM GCC $(/bin/date)"  -m "Build completed on: $(/bin/date)" -m "Configuration: $(/bin/cat /tmp/gcc-arm-version)" -m "LLD: $(/bin/cat /tmp/lld-arm-version)"
+git commit -as -m "Release ARM64 GCC $(/bin/date)"  -m "Build completed on: $(/bin/date)" -m "Configuration: $(/bin/cat /tmp/gcc-version)" -m "LLD: $(/bin/cat /tmp/lld-arm64-version)"
 git gc
 git push origin gcc-master -f
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="$GCC_NAME GCC arm64: Toolchain compilation Finished</b>%0A<b>Gcc Version : </b><code>$(/bin/cat /tmp/gcc-version)"</code>%0A<b>LLD Version : </b><code>$(/bin/cat /tmp/lld-arm64-version)</code>"
+curl -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d "disable_web_page_preview=true" -d "parse_mode=html" -d text="<b>$GCC_NAME GCC arm64: Toolchain pushed to <code>https://github.com/"${GITHUB_USER}"/gcc-arm64</code></b>"
